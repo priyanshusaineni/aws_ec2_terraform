35b2530e0d7365622a2d78a24b46f284136a3a0d
priyanshusaineni <priyanshusaineni2004@gmail.com>
2025-11-03 11:15:43 +0530

    Updated outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-# output "ec2_instance_id" {
-#   value = module.ec2_instance[0].id
-#   description = "The EC2 instance ID from the module"
-# }
+output "ec2_instance_id" {
+  value = module.ec2_instance[0].id
+  description = "The EC2 instance ID from the module"
+}

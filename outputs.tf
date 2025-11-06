685457e9848a6b7d9b846ebb48e7b8397642cc0a
priyanshusaineni <priyanshusaineni2004@gmail.com>
2025-11-06 16:19:25 +0530

    Removed sensitive output vallues in outputs.tf file

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,3 @@ output "ec2_instance_id" {
   value = module.ec2_instance[0].id
   description = "The EC2 instance ID from the module"
 }
-
-output "git_token" {
-  value = var.git_token
-  description = "Git token value"
-}
